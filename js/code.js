71abfdfbb7357cf2cc1bf9906c6f6ade0cfef824
Bradigson <71954800+Bradigson@users.noreply.github.com>
2021-10-01 21:32:15 -0400

    done

---
+++ js/code.js
@@ -0,0 +1,5 @@
+let menuIcon = document.querySelector(".hamburger-menu");
+let navbar = document.querySelector(".navbar");
+menuIcon.addEventListener("click",()=>{
+    navbar.classList.toggle("change");
+});
